78ed18950595a0596d3b5a5dfc120a3d7a053084
WoolKnitCap <woolknitcap@outlook.com>
2020-09-02 14:54:33 -0400

    getUserInput function.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function getUserInput() {
+  return parseInt(userInput.value);
 }
 
-currentResult = add(1, 2);
+function add() {
+  const userNum = getUserInput();
+  const calcDesc = `${currentResult} + ${userNum}`;
+  currentResult += userNum;
+  outputResult(currentResult, calcDesc);
+}
 
-let calculationDesc = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDesc);
+addBtn.addEventListener('click', add);
