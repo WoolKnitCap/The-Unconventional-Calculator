669c0ffaf369fafd54612457417c4b851549d759
WoolKnitCap <woolknitcap@outlook.com>
2020-09-02 17:35:27 -0400

    getUserInput function.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,40 @@ function getUserInput() {
   return parseInt(userInput.value);
 }
 
-function add() {
-  const userNum = getUserInput();
-  const calcDesc = `${currentResult} + ${userNum}`;
-  currentResult += userNum;
+function writeOutput(operator, resultBefore, calcNum) {
+  const calcDesc = `${resultBefore} ${operator} ${calcNum}`;
   outputResult(currentResult, calcDesc);
 }
 
+function add() {
+  const userNum = getUserInput();
+  const initResult = currentResult;
+  currentResult += userNum;
+  writeOutput('+', initResult, userNum);
+}
+
+function sub() {
+  const userNum = getUserInput();
+  const initResult = currentResult;
+  currentResult -= userNum;
+  writeOutput('-', initResult, userNum);
+}
+
+function multi() {
+  const userNum = getUserInput();
+  const initResult = currentResult;
+  currentResult *= userNum;
+  writeOutput('*', initResult, userNum);
+}
+
+function div() {
+  const userNum = getUserInput();
+  const initResult = currentResult;
+  currentResult /= userNum;
+  writeOutput('/', initResult, userNum);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', sub);
+multiplyBtn.addEventListener('click', multi);
+divideBtn.addEventListener('click', div);
